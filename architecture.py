2bafb78f20a83ce931c5fe53671d8944f192cdf4
Medha-Iyer <contactmedha10@gmail.com>
2020-08-05 16:01:37 -0700

    Updated CNN with layers; ready to use as module

--- architecture.py
+++ architecture.py
@@ -4,88 +4,101 @@ Created on Tue Aug  4 20:26:23 2020
 @author: medha iyer
 """
 
-# #import keras
-# from keras.models import Sequential
-# from keras.layers import Dense
-#
-# # define three sets of inputs
-# spiral_test = Input()
-# meander_test = Input()
-# circle_test = Input()
-#
 
 import torch
 from torch import nn
 
-class TwoInputsNet(nn.Module):
-  def __init__(self):
-    super(TwoInputsNet, self).__init__()
-    self.conv = nn.Conv2d( ... )  # set up your layer here
-    self.fc1 = nn.Linear( ... )  # set up first FC layer
-    self.fc2 = nn.Linear( ... )  # set up the other FC layer
+class SimpleConv(nn.Module):
+  def __init__(self, num_classes, spiral_size, meander_size, circle_size):
+      super(SimpleConv, self).__init__()
+      self.spiral_size = spiral_size
+      self.meander_size = meander_size
+      self.circle_size = circle_size
+      self.dim = 0 #dimensions of image after concatenation (set in forward())
 
-  def forward(self, input1, input2):
-    c = self.conv(input1)
-    f = self.fc1(input2)
-    # now we can reshape `c` and `f` to 2D and concat them
-    combined = torch.cat((c.view(c.size(0), -1),
-                          f.view(f.size(0), -1)), dim=1)
-    out = self.fc2(combined)
-    return out
+      self.spiral_nn = nn.Sequential(
+          nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+          nn.ReLU(inplace=True),
+          nn.MaxPool2d(kernel_size=3, stride=2),
+          nn.Conv2d(64, 192, kernel_size=5, padding=2),
+          nn.ReLU(inplace=True),
+          nn.MaxPool2d(kernel_size=3, stride=2),
+          nn.Conv2d(192, 384, kernel_size=3, padding=1),
+          nn.ReLU(inplace=True),
+          nn.Conv2d(384, 256, kernel_size=3, padding=1),
+          nn.ReLU(inplace=True),
+          nn.Conv2d(256, 256, kernel_size=3, padding=1),
+          nn.ReLU(inplace=True),
+          nn.MaxPool2d(kernel_size=3, stride=2),
+      )
 
-  class SimpleConv(nn.Module):
-      def __init__(self, num_classes):
-          super(SimpleConv, self).__init__()
-          self.features = nn.Sequential(
-              nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-              nn.ReLU(inplace=True),
-              nn.MaxPool2d(kernel_size=3, stride=2),
-              nn.Conv2d(64, 192, kernel_size=5, padding=2),
-              nn.ReLU(inplace=True),
-              nn.MaxPool2d(kernel_size=3, stride=2),
-              nn.Conv2d(192, 384, kernel_size=3, padding=1),
-              nn.ReLU(inplace=True),
-              nn.Conv2d(384, 256, kernel_size=3, padding=1),
-              nn.ReLU(inplace=True),
-              nn.Conv2d(256, 256, kernel_size=3, padding=1),
-              nn.ReLU(inplace=True),
-              nn.MaxPool2d(kernel_size=3, stride=2),
-          )
-          self.classifier = nn.Sequential(
-              nn.Dropout(),
-              nn.Linear(256 * 6 * 6, 4096),
-              nn.ReLU(inplace=True),
-              nn.Dropout(),
-              nn.Linear(4096, 4096),
-              nn.ReLU(inplace=True),
-              nn.Linear(4096, num_classes),
-          )
+      self.meander_nn = nn.Sequential(
+          nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+          nn.ReLU(inplace=True),
+          nn.MaxPool2d(kernel_size=3, stride=2),
+          nn.Conv2d(64, 192, kernel_size=5, padding=2),
+          nn.ReLU(inplace=True),
+          nn.MaxPool2d(kernel_size=3, stride=2),
+          nn.Conv2d(192, 384, kernel_size=3, padding=1),
+          nn.ReLU(inplace=True),
+          nn.Conv2d(384, 256, kernel_size=3, padding=1),
+          nn.ReLU(inplace=True),
+          nn.Conv2d(256, 256, kernel_size=3, padding=1),
+          nn.ReLU(inplace=True),
+          nn.MaxPool2d(kernel_size=3, stride=2),
+      )
 
-      def forward(self, spirals, meanders, circles):
-          spirals = self.features(spirals)
-          spirals = spirals.view(spirals.size(0), 256 * 6 * 6) #TODO find what this line does
-          spirals = self.classifier(spirals)
+      self.circle_nn = nn.Sequential(
+          nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+          nn.ReLU(inplace=True),
+          nn.MaxPool2d(kernel_size=3, stride=2),
+          nn.Conv2d(64, 192, kernel_size=5, padding=2),
+          nn.ReLU(inplace=True),
+          nn.MaxPool2d(kernel_size=3, stride=2),
+          nn.Conv2d(192, 384, kernel_size=3, padding=1),
+          nn.ReLU(inplace=True),
+          nn.Conv2d(384, 256, kernel_size=3, padding=1),
+          nn.ReLU(inplace=True),
+          nn.Conv2d(256, 256, kernel_size=3, padding=1),
+          nn.ReLU(inplace=True),
+          nn.MaxPool2d(kernel_size=3, stride=2),
+      )
 
-          meanders = self.features(meanders)
-          meanders = meanders.view(meanders.size(0), 256 * 6 * 6)
-          meanders = self.classifier(meanders)
+      self.concat_nn = nn.Sequential(
+          nn.Dropout(),
+          nn.Linear(self.dim, 4096),
+          nn.ReLU(inplace=True),
+          nn.Dropout(),
+          nn.Linear(4096, 4096),
+          nn.ReLU(inplace=True),
+          nn.Linear(4096, num_classes),
+      )
 
-          circles = self.features(circles)
-          circles = circles.view(circles.size(0), 256 * 6 * 6)
-          circles = self.classifier(circles)
+  def forward(self, spirals, meanders, circles):
+      spirals = self.spiral_nn(spirals)
+      spirals = spirals.view(spirals.size(0), -1)
 
-          # now we can reshape these to 2D and concat them
-          # TODO find what view we need to reshape to based on how we want it to output
-          combined = torch.cat((c.view(c.size(0), -1),
-                                f.view(f.size(0), -1)), dim=1)
-          out = self.fc2(combined)
+      meanders = self.meander_nn(meanders)
+      meanders = meanders.view(meanders.size(0), -1)
 
-          return out
+      circles = self.circle_nn(circles)
+      circles = circles.view(circles.size(0), -1)
+
+      # now we can concatenate them
+      combined = torch.cat((spirals, meanders, circles), dim=1)
+      self.dim = combined.shape()[1]
+      out = self.concat_nn(combined)
+
+      return out
 
 # When we import as a module in another file, we can declare SimpleConv variable
 # num_classes is the size of each input sample (how many samples are we breaking training data into?)
-  def simple_conv(pretrained=False, num_classes=140):
-      model = SimpleConv(num_classes)
-      # if pretrained:
-      #     model.load_state_dict(model_zoo.load_url(model_urls['alexnet']))
-      return model
+#   def simple_conv(pretrained=False, num_classes=2):
+#       model = SimpleConv(num_classes, spiral_size=756*786, meander_size=744*822, circle_size=675*720)
+#       return model
+
+# dimensions = {"Meander": (744,822), "Spiral":(756,786),"Circle":(675,720)}
+
+if __name__ =='__main__':
+    num_classes = 2
+    model = SimpleConv(num_classes, spiral_size=756*786, meander_size=744*822, circle_size=675*720)
