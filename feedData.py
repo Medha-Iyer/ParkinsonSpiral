ef185cfb315679c773280400db044e09677c0f0f
Adam <awasserman21@scarsdaleschools.org>
2020-08-07 17:59:30 -0400

    using GPUs

--- feedData.py
+++ feedData.py
@@ -5,19 +5,17 @@ import torch
 from torch import nn
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
 
-epochs = sys.argv[2] #what value should we set this
-batch_size = sys.argv[3]
+epochs = 100 #what value should we set this
+batch_size = 10
 threshold = 0.5
-run_num = sys.argv[1]
+run_num = 1
 losses = []
 accs = []
 precision = []
 recall = []
 f1 = []
 
-print(run_num)
 conf_mat = np.zeros((2,2), dtype = 'i')
 
 X_train,X_test,y_train,y_test = getData.getData('/Users/adamwasserman/Documents/RISE/Project')
@@ -29,7 +27,10 @@ data_loader = torch.utils.data.DataLoader(dataset, batch_size, shuffle=True)
 # train_spiral_loader = torch.DataLoader(train_spirals, batch_size)
 # train_circle_loader = torch.DataLoader(train_circles, batch_size)
 
+device=torch.device('cuda:0')
 NN = SimpleConv(num_classes=1,size = (756,822)) #hardcoded for now
+NN.to(device)
+
 #TODO maybe set these as default values in constructor
 
 optimizer = torch.optim.Adam(params=NN.parameters(), lr=0.05) #TODO ask about lr
@@ -37,13 +38,15 @@ torch.optim.lr_scheduler.StepLR(optimizer, step_size = 10, gamma=0.1, last_epoch
 cost_func = nn.BCELoss()
 
 for i in range (epochs):
-    for j, data in enumerate(data_loader):
-        yhat = NN.forward(data[0][:, 0], data[0][:, 1], data[0][:, 2]).reshape(5) #reshaped to 5
-        loss = cost_func(yhat, data[1])
+    for j, (X,y) in enumerate(data_loader):
+        X = X.to(device)
+        y = y.to(device)
+        yhat = NN.forward(X[:, 0], X[:, 1], X[:, 2]).reshape(5) #reshaped to 5
+        loss = cost_func(yhat, y)
         yhat = (yhat>threshold).float()
-        acc = torch.eq(yhat.round(), data[1]).float().mean()  # accuracy
+        acc = torch.eq(yhat.round(), y).float().mean()  # accuracy
         
-        for pred,actual in zip(yhat,data[1]):
+        for pred,actual in zip(yhat,y):
             conf_mat[actual,pred] += 1
         
         optimizer.zero_grad()
